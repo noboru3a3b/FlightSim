cfe4e9537512b8c759d9de03077958ba747e0b8e
Noboru Okada <69345043+noboru3a3b@users.noreply.github.com>
2026-05-30 09:09:09 +0900

    Add files via upload

---
+++ MeatEngine/Widgets/__init__.py
@@ -0,0 +1,26 @@
+"""Meat Engine Widget Set
+
+The term "Widget" in my context is similar to the use of the term in
+the X windows system - a control used to build a GUI.
+
+Widgets may be interactive (buttons, sliders) or non-interactive (text
+displays, separators, images). Widgets may be containers for other
+widgets (sizers, scroll boxes).
+
+This library is intended to be display-neutral, but my principle
+interest is drawing to OpenGL.
+
+TODO List
+
+ - pushbutton
+ - radio button
+ - checkbox
+ - static text box
+ - editable text box
+ - multiline text box (editable and non)
+ - scroll bar
+ - scrollable container with clipping
+ - separator
+ - grid sizer
+
+"""
